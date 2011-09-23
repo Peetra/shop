9a62d9db5a6b7ac7bbe8eec49b55c1dacf726f64
Petra Brandt <peetra.mammapappa@gmail.com>
2011-09-23 13:44:01 +0300

    misc

--- functions.php
+++ functions.php
@@ -1,3 +1,12 @@
 <?php
-
+function where_am_i($file_name)
+{
+	$who_am_i = $_SERVER['PHP_SELF'];
+	if (basename($who_am_i, ".php") == $file_name)
+		{
+			include ('./styles/default/footer.html');
+			echo '^_^';
+		}
+}
+where_am_i('functions');
 ?>
