4ec1d1a5db156bd65b9c29f8ed138951fa4a7822
Petra Brandt <peetra.mammapappa@gmail.com>
2011-09-24 13:10:31 +0300

    checking forms and css

--- adm/pages.php
+++ adm/pages.php
@@ -9,13 +9,13 @@ echo '<div id="acp-top-explanation"><h3>' . $lang['ACP_PAGES_TITLE'] . '</h3>';
 echo '<p>' . $lang['ACP_PAGES_EXPLAIN'] . '</p></div>';
 
 ?>
-	<menu class="side">
+	<div id="sidemenu">
 	<ul>
 	<li><a href="./pagesEdit.php"><?php echo $lang['EDIT_PAGE']?></a></li>
 	<li><a href="./pagesAdd.php"> <?php echo $lang['NEW_PAGE_MENU']?></a></li>
 	<li><a href="./pagesDelete.php"><?php echo $lang['DEL_PAGE']?></a></li>
 	</ul>
-	</menu>
+	</div>
 <?php
 if (basename($who_am_i, ".php") == 'pages') // basename returns the end of a path, here we exclude the fileextension
 {
